0f6b9a61a4834463374b642e43dad38f9f23f732
Sampo Lahtinen <lampo.sahtinen@gmail.com>
2018-04-24 16:29:07 +0200

    finished

--- src/Book.js
+++ src/Book.js
@@ -12,25 +12,29 @@ class Book extends React.Component {
     state = {
         shelf: this.props.book.shelf,
     }
+
+    componentWillMount = () => {
+        if (this.props.book.imageLinks === undefined) this.props.book.imageLinks = { thumbnail: "no cover"}
+    }
+
     render() {
         return (
             <div className='book'>
-                <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}>
-                    <div className='book-shelf-changer' >
-                        <div className='book-shelf-menu'>
-                            <select defaultValue={this.state.shelf} onChange={(event)=>{
-                                this.props.updateShelf(this.props.book, event.target.value)
-                                this.setState({shelf: event.target.value})
-                            }}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
+                <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`, backgroundColor: "grey"}}>
+                <div className='book-shelf-changer' >
+                    <div className='book-shelf-menu'>
+                        <select defaultValue={this.state.shelf} onChange={(event)=>{
+                            this.props.updateShelf(this.props.book, event.target.value)
+                            this.setState({shelf: event.target.value})
+                        }}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
-                    
+                </div>
                 </div>
                 <div className='book-info-wrapper'>
                     <h3 className='book-title'>{this.props.book.title}</h3>
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -15,7 +15,8 @@ class SearchBooks extends React.Component {
     }
     state = {
         query: this.props.firstQuery,
-        books: []
+        books: [],
+        validQuery: true
     }
     componentDidMount = () => {
         if(this.state.query === '') return
@@ -26,7 +27,7 @@ class SearchBooks extends React.Component {
         SearchBooks.forEach(book => {
             //Find returns the first matched element from the array it is called on. IF not found, return undefined
             let selectedBook = this.props.selectedBooks.find( userBook => userBook.id === book.id  )
-            console.log(selectedBook)
+            //console.log(selectedBook)
             if(selectedBook) {
                 book.shelf = selectedBook.shelf
             } else {
@@ -34,67 +35,81 @@ class SearchBooks extends React.Component {
             }
         })
     }
-    updateQuery = (query) => {
-        this.setState({query: query.trim()});
-        if (this.validateSearchQuery(this.state.query)) {
-            this.searchBooks(query);
-        } else {
-            alert("Not valid search term!")
-            this.setState({query: ''})
-        }
-        
-    }
     searchBooks = (query) => {
-        if(query === '') return
-        BooksAPI.search(query).then((books)=>{
+        if (query === '') return
+        BooksAPI.search(query).then((books) => {
+            books.forEach((book) => { if(book.imageLinks === undefined) book.imageLinks = { thumbnail: "no cover" }})
             this.assignShelf(books);
-            this.setState({books: books});
-        })
+            this.setState({
+                books: books
+            });
+        }).catch(error => console.log(error))
     }
     validateSearchQuery = (query) => {
+        //console.log("query at validateSearchQuery function: " + query)
         let match = new RegExp(query, 'i')
-        let filteredTerms = searchTerms.searchTerm.filter( term => match.test(term) )
-        if (filteredTerms.length > 0) { 
+        let filteredTerms = searchTerms.searchTerm.filter(term => match.test(term))
+        if (filteredTerms.length > 0) {
+            this.setState({
+                validQuery: true
+            })
             return true
         } else {
+            this.setState({
+                validQuery: false
+            })
             return false
         }
     }
-
+    updateQuery = (query) => {
+        //console.log("query at updateQuery function: " + query)
+        this.setState({query: query});
+        if (this.validateSearchQuery(query)) {
+            //console.log("Result of validateSearcQuery: " + this.validateSearchQuery(query))
+            this.searchBooks(query);
+        } else { return }
+    }
+    
     render() {
-        return <div> 
-            <div className="search-field-wrapper">
-              <input 
-                className="search-field" 
-                type="text" 
-                placeholder="Search new books..." 
-                value={this.state.query} //defaultValue={this.props.firstQuery}
-                onChange={event => this.updateQuery(event.target.value)} />
+        return (
+            <div>
+                <div className="search-field-wrapper">
+                    <input
+                        className="search-field"
+                        type="text"
+                        placeholder="Search new books..."
+                        value={this.state.query}
+                        onChange={event => this.updateQuery(event.target.value)}/>
+                </div>
+                <Link to="/">
+                    <div className="back-arrow">
+                        <img alt="arrow back icon" src={arrowBack} />
+                    </div>{" "}
+                </Link>
+                <div className="book-list">
+                    <section className="search-books">
+                        <h2 className="section-title">Search Results</h2>
+                        <hr />
+                        {this.state.query !== "" && this.state.validQuery === true &&
+                            <ol className="books-list">
+                                {this.state.books.length > 0 && this.state.books.map(
+                                    book => (
+                                        <li key={book.id} className="book-list-item">
+                                            <Book
+                                                book={book}
+                                                updateShelf={this.props.updateShelf}
+                                            />
+                                        </li>
+                                    )
+                                )}
+                            </ol>
+                        }
+                        {this.state.validQuery === false && <h3 className="no-results">No results :(...</h3>}
+                    </section>
+                </div>
             </div>
-            <Link to="/">
-              <div className="back-arrow">
-                <img alt="arrow back icon" src={arrowBack} />
-              </div>{" "}
-            </Link>
-            {this.state.query !== "" && <div className="book-list">
-                <section className="search-books">
-                  <h2 className="section-title">Search Results</h2>
-                  <hr />
-                  <ol className="books-list">
-                    {this.state.books.length > 0 && this.state.books.map(
-                        book => (
-                          <li key={book.id} className="book-list-item">
-                            <Book
-                              book={book}
-                              updateShelf={this.props.updateShelf}
-                            />
-                          </li>
-                        )
-                      )}
-                  </ol>
-                </section>
-              </div>}
-          </div>;}
+        )
+        }
 }
 
 export default SearchBooks
